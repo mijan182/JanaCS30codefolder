f11cace9d177cd9c403385e70a4df10d21ef0e29
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-16 09:23:59 -0600

    finished A4

--- A4-ASCIIart/src/App.java
+++ A4-ASCIIart/src/App.java
@@ -1,14 +1,23 @@
+/*Project name: A4-ASCII ART
+ * Student name: Jana Marie Reyes
+ * Date: 9/16/24
+ * Program description: Draw a creature/animal using ASCII art
+*/
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("/\\_________/\\");
+        System.out.println("                    ");
+        System.out.println(" /\\---------/\\");
         System.out.println("(   `.    `.  )");
-        System.out.println(" )      ^    (");
-        System.out.println(" /             \\");
-        System.out.println("|               )");
-        System.out.println("(              /");
-        System.out.println(" |            |");
+        System.out.println(" )      `    (   \\\\|//"); //cat with a dandilion
+        System.out.println(" /             \\   /");
+        System.out.println("(  )            )_/");
+        System.out.println(" ``           ``/");
+        System.out.println(" |      ,     |");
         System.out.println(" |      |     |");
         System.out.println(" |      \\     \\");
         System.out.println(" (_______)______)");
+        System.out.println("                    ");
     }
 }

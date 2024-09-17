3525f6218f1be4c85d0a1715c047cf7613c3c99f
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-17 09:49:08 -0600

    demovariable

---
+++ 1-DemosandEx/LessonVariables/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //casting
+        // System.out.println(1 / 3);
+        // System.out.println(1.0 / 3);
+        // System.out.println(1 / 3.0);
+        // System.out.println(1.0 / 3.0);
+        // System.out.println((double) 1 / 3); 
+
+        //rollover
+        // int myNum = 2147483647;
+        // System.out.println(myNum);
+        // myNum = myNum +1;
+        // System.out.println(myNum);
+        // System.out.println(Integer.MAX_VALUE);
+
+
+        // int num = Math.random();
+        // System.out.println(num);
+
+        System.out.println((int)(Math.random()*10));
+    }
+}

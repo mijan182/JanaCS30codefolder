1bfcdb7cc5bfb94016886896a6da3792523908b0
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-18 09:35:12 -0600

    updateA6

---
+++ A6-VariableBasics/src/App.java
@@ -0,0 +1,14 @@
+/*Program name: Variable Basics
+ * Name: Jana Marie Reyes
+ * Date: 9/19/24
+ * Program Description: Write a code to print the number of chicken 
+ * wings if you have $4.75 and they cost $0.75 each. 
+ * Remember that you can’t buy part of a wing.*/
+ 
+public class App {
+    public static void main(String[] args) throws Exception {
+        double y = 4.75 / 0.75;
+        int x = ;
+        System.out.println(x);
+    }
+}

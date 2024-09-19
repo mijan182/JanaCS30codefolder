c12b9a79989761acce5805bf33de74c207c49212
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-18 23:04:34 -0600

    FINISHED A6
    (changed the date)

--- A6-VariableBasics/src/App.java
+++ A6-VariableBasics/src/App.java
@@ -1,6 +1,6 @@
 //Program name: Variable Basics
 //Name: Jana Marie Reyes
-//Date: 9/19/24
+//Date: 9/18/24
 //Program Description: Write a code to print the number of chicken 
 //wings if you have $4.75 and they cost $0.75 each. 
 //Remember that you can’t buy part of a wing.

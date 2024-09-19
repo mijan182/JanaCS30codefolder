1e11cbef9a62df07762ef874d709bd0baae6f03a
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-18 23:00:30 -0600

    Finished A6

--- A6-VariableBasics/src/App.java
+++ A6-VariableBasics/src/App.java
@@ -1,14 +1,15 @@
-/*Program name: Variable Basics
- * Name: Jana Marie Reyes
- * Date: 9/19/24
- * Program Description: Write a code to print the number of chicken 
- * wings if you have $4.75 and they cost $0.75 each. 
- * Remember that you can’t buy part of a wing.*/
+//Program name: Variable Basics
+//Name: Jana Marie Reyes
+//Date: 9/19/24
+//Program Description: Write a code to print the number of chicken 
+//wings if you have $4.75 and they cost $0.75 each. 
+//Remember that you can’t buy part of a wing.
  
 public class App {
     public static void main(String[] args) throws Exception {
-        double y = 4.75 / 0.75;
-        int x = ;
-        System.out.println(x);
+        double Money = 4.75;
+        double Cost = 0.75;
+        int numofchickenwings = (int)(Money / Cost);
+        System.out.println("The total amount of chicken wings you can purchase is " + numofchickenwings + " wings.");
     }
 }

a5069f00c76c7cb39b54a77faadc6230691c1157
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-25 09:45:36 -0600

    started A8B assignment

---
+++ A8b-Passing Methods/src/App.java
@@ -0,0 +1,11 @@
+// Program name: Passing Methods
+// Student name: Jana Marie Reyes
+// Date: 9/25/24
+// Program Description: Create a sub class with two methods. reate a runner class that calls those methods and shows an output.
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}

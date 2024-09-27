a160ad811ae6aaec7faae8cc8a9891dff3104769
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-26 23:23:13 -0600

    i finished A8B i think

---
+++ A8b-Passing Methods/src/Cat.java
@@ -0,0 +1,16 @@
+import static java.lang.System.*;
+
+public class Cat 
+{
+    public void speak(){
+        out.println("meow meow meow");
+    }
+
+    public void name(){
+        out.println("im meowmoew");
+    }
+
+    public void garfield(){
+        out.println("i love lasagna");
+    }
+}
--- A8b-Passing Methods/src/catRunner.java
+++ A8b-Passing Methods/src/catRunner.java
@@ -1,11 +1,16 @@
 // Program name: Passing Methods
 // Student name: Jana Marie Reyes
-// Date: 9/25/24
+// Date: 9/26/24
 // Program Description: Create a sub class with two methods. reate a runner class that calls those methods and shows an output.
 
 
-public class App {
+public class catRunner {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cat meow = new Cat();
+        meow.speak();
+        meow.name();
+        meow.speak();
+        meow.garfield();
+        meow.speak();
     }
 }

7ce4dbbd1f8bc37df6b8b1989787bb0911b35223
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-27 09:18:59 -0600

    update

--- A8b-Passing Methods/src/catRunner.java
+++ A8b-Passing Methods/src/catRunner.java
@@ -4,7 +4,7 @@
 // Program Description: Create a sub class with two methods. reate a runner class that calls those methods and shows an output.
 
 
-public class catRunner {
+public class CatRunner {
     public static void main(String[] args) throws Exception {
         Cat meow = new Cat();
         meow.speak();

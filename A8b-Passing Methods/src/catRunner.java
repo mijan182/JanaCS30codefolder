f70cba840c9859a41e36e9e9d2f34b800fff44cb
mijan182 <jana-marie.reyes@rcsd.ca>
2024-09-27 09:37:47 -0600

    okay i've finished A8b
    genuinely

--- A8b-Passing Methods/src/catRunner.java
+++ A8b-Passing Methods/src/catRunner.java
@@ -1,6 +1,6 @@
 // Program name: Passing Methods
 // Student name: Jana Marie Reyes
-// Date: 9/26/24
+// Date: 9/27/24
 // Program Description: Create a sub class with two methods. reate a runner class that calls those methods and shows an output.
 
 

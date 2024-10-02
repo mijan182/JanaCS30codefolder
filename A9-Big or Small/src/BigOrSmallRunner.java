9d46ea5d449ac86b9638a9581be352ddf039817e
mijan182 <jana-marie.reyes@rcsd.ca>
2024-10-02 09:45:21 -0600

    A9 not done
    not done

--- A9-Big or Small/src/BigOrSmallRunner.java
+++ A9-Big or Small/src/BigOrSmallRunner.java
@@ -7,8 +7,14 @@
 // Program Description: Write a program to check 2 parameters.  If a is > b, output "yes".  If b is >
 //a, output "no".  If a is equal to b, return "equals".
 
+import java.util.Scanner;
+
 public class BigOrSmallRunner {
     public static void main(String[] args) throws Exception {
-        
+
+        // if (int a > int b){
+        //     System.out.println("yes");
+        }
+        }
     }
 }

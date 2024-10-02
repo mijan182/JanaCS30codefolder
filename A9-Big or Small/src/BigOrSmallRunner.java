16d3733a39c60402529844291461f9687789ac7f
mijan182 <jana-marie.reyes@rcsd.ca>
2024-10-02 09:28:35 -0600

    started A9

---
+++ A9-Big or Small/src/BigOrSmallRunner.java
@@ -0,0 +1,14 @@
+// Program name: Big or Small
+
+// Student name: Jana Marie Reyes
+
+// Date: 10/./24
+
+// Program Description: Write a program to check 2 parameters.  If a is > b, output "yes".  If b is >
+//a, output "no".  If a is equal to b, return "equals".
+
+public class BigOrSmallRunner {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}

caf6bc7cb18424dc0bcf0177dc5be0b0f0139c2a
mijan182 <jana-marie.reyes@rcsd.ca>
2024-10-08 09:42:41 -0600

    A12 unfinished

---
+++ A12-Fahrenheit to Celsius/src/Fahrenheit.java
@@ -0,0 +1,18 @@
+public class Fahrenheit {
+
+    private double FahrenNum;
+
+    public void setfahrenheit(double FahrenNum){
+        
+    }
+    public double getcelcius(){
+        double celcius = 0.0;
+        celcius = (( FahrenNum -32)*5/9);
+        return (celcius);
+    }
+
+    public void print(){
+        System.out.printf("Celcius: %.1f \n", getcelcius());
+        
+    }
+}
---
+++ A12-Fahrenheit to Celsius/src/Fahrenheitrunner.java
@@ -0,0 +1,9 @@
+public class  Fahrenheitrunner {
+    public static void main(String[] args) throws Exception {
+        Fahrenheit test = new Fahrenheit();
+
+        test.setfahrenheit(200);
+        test.print();
+
+    }
+}

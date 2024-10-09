a885d0990dbc7aa9341d7932d9a49005beabea22
mijan182 <jana-marie.reyes@rcsd.ca>
2024-10-09 08:55:13 -0600

    A12:forgot to add the comment header but i really am finished

--- A12-Fahrenheit to Celsius/src/Fahrenheit.java
+++ A12-Fahrenheit to Celsius/src/Fahrenheit.java
@@ -1,3 +1,8 @@
+// Program name: Fahrenheit to Celcius
+// Student name: Jana Marie Reyes
+// Date: 10/9/24
+// Program Description: Convert Celsius to Fahrenheit.   
+
 public class Fahrenheit {
 
     private double FahrenNum;

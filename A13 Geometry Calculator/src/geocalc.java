80081d43230c135ff310b7ab2585c4d3dc825751
mijan182 <jana-marie.reyes@rcsd.ca>
2024-10-10 01:19:34 -0600

    A12 finished
    added comments

--- A13 Geometry Calculator/src/geocalc.java
+++ A13 Geometry Calculator/src/geocalc.java
@@ -1,18 +1,21 @@
 
 
-
+//The different calculators
 public class geocalc {
+   //perimeter of a rectangle
      public void rectangle (double length , double width){
         double perimeter = 2*(length + width);
         System.out.printf("The perimeter of the rectangle is %.2f", perimeter);
      }
+     //SA of a cube 
      public void cube (double side){
         double surfacearea = 6*(side*side);
         System.out.printf("The surface area of the cube is %.2f", surfacearea);
      }
+     //perimeter of a rectangle
      public void circle (double radius){
         double pi = 3.14;
         double areacircle = pi*(radius*radius);
-        System.out.printf("The surface area of the cube is %.2f", areacircle);
+        System.out.printf("The surface area of the cube is %.2f", areacircle); //%.2f to output answer with 2 decimal places
      }
 }
--- A13 Geometry Calculator/src/geocalcrunner.java
+++ A13 Geometry Calculator/src/geocalcrunner.java
@@ -2,16 +2,19 @@
 // Student name: Jana Marie Reyes
 // Date: 10/9/24
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
+//asks for user input rather than using cases 
 public class geocalcrunner {
     public static void main(String[] args) throws Exception {
+        //initializes new terms (if thats how you phrase it)
         Scanner input = new Scanner(System.in);
         geocalc compute = new geocalc();
 
             System.out.println("What do you wish to calculate? \n A. Perimeter of a rectangle \n B. Surface area of a cube \n C. Area of a circle \n (please answer with the corresponding Capital letter.) \n");
             String choice = input.next();
             
+            //using if else statements to respond to text response of user
             if (choice.equals("A")){
                 System.out.println("Input the width: ");
                 double width = input.nextDouble();
@@ -34,7 +37,7 @@ public class geocalcrunner {
                 
                 compute.circle(radius);
             }
-
+            //closes scanner
             input.close();
     }
 }

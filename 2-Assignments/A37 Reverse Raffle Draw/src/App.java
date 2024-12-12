427a15e8acb8e663112caa44e30a747647b935a0
mijan182 <jana-marie.reyes@rcsd.ca>
2024-12-12 09:47:25 -0600

    37

--- 2-Assignments/A37 Reverse Raffle Draw/src/App.java
+++ 2-Assignments/A37 Reverse Raffle Draw/src/App.java
@@ -11,7 +11,10 @@ public class App {
         List<String> nameList = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please input your 10 chosen names!\n");
+        System.out.println("The Reverse Raffle Draw\n");
+        System.out.println("A reverse raffle is a raffle draw where the name drawn\nfrom a hat they DOES NOT win. The last name drawn is the actual winner.\n");
+        System.out.println("For this, we'll have 10 names inputted into the hat,\nand for every name drawn, they get removed from the list.\nLast name standing wins!");
+
 
         //loop to get user input
         while (nameList.size() < 10) {
@@ -19,11 +22,12 @@ public class App {
             String names = input.nextLine();
             nameList.add(names);
 
-            System.out.println("\nYou entered the following names: ");
+            System.out.print("\nYou entered the following names: ");
         for (String name : nameList) {
-            System.out.println(name);
+            System.out.print(name + " ");
 
         }
+        System.out.println();
     }
     }
 }
